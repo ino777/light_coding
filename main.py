3124a62b62639b195f826823961d570c9ba1ac47
Manato Inoue <tarou.xxx.0129@gmail.com>
2020-08-14 22:31:49 +0900

    微修正

--- main.py
+++ main.py
@@ -3,21 +3,4 @@ from app import webserver
 
 
 if __name__ == '__main__':
-#     code = ('''\
-# # This file is a "Hello, world!" in Python language by CPython for wandbox.
-
-# import json
-
-# def f():
-#     print('This is America!')
-
-# d = json.dumps({'aa': 1})
-# print("Hello, world!", d)
-# f()
-
-# # CPython references:
-# #   https://www.python.org/
-#     ''')
-#     response = wandbox.compile(code)
-#     print(response)
     webserver.start_webserver()
